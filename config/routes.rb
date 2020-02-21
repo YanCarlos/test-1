5c6125945b0712b979a17d9dd46fe370080b4475
Yan  Carlos Marín Osorio <yancarlosmarinosorio@gmail.com>
2020-02-20 21:32:58 -0500

    Building routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, constraints: { format: :json } do
+    namespace :v1 do
+      resources :artists, only: :index do
+        member do
+          resources :albums, only: :index
+        end
+      end
+
+      resources :albums, only: [] do
+        member do
+          resources :songs, only: :index
+        end
+      end
+
+      resources :genres, param: :genre_name, only: [] do
+        member do
+          get :random_song
+        end
+      end
+    end
+  end
 end
